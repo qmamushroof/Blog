b95e06d5964c18db095ba37de2a004772f5898ed
Quazi Mushroof Abdullah <quazi.abdullah@studynetglobal.com>
2025-12-12 14:46:44 +0600

    Add Post and Comment models with data annotations
    Introduced Post and Comment classes in the Blog.Models namespace. Both models use data annotations for validation and database mapping, including keys, required fields, string length limits, and foreign key relationships. Post includes navigation properties for related comments, while Comment references its parent post.

---
+++ Blog/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Content { get; set; }
+        public string? Author { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey(nameof(Post))]
+        public int PostId { get; set; }
+        [ValidateNever]
+        public Post? Post { get; set; }
+    }
+}
---
+++ Blog/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string? Title { get; set; }
+        [Required]
+        public string? Content { get; set; }
+        public string? Author { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey(nameof(Comment))]
+        public List<Comment>? Comments { get; set; }
+        [ValidateNever]
+        public Comment? Comment { get; set; }
+    }
+}
