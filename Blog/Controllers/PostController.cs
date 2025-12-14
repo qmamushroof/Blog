1f000ae69bb2f74cb598b3035f3523b877f3835f
qmamushroof <qmamushroof@gmail.com>
2025-12-14 08:49:34 +0600

    Add PostController with CRUD actions and view folder setup
    Added PostController with basic Index, Create, Edit, and Delete action methods using ApplicationDbContext. Updated Blog.csproj to include the Views\Post\ folder for organizing related view files. No database logic implemented yet.

---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,54 @@
+﻿using Blog.Data;
+using Blog.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Post post)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Post post)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            return View();
+        }
+    }
+}
