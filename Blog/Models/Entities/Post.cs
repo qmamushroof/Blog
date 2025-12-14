71cedd974deb5554d3d891c0dcad04f389700991
qmamushroof <qmamushroof@gmail.com>
2025-12-14 21:01:28 +0600

    Remove unused using directives and clean up namespaces
    Removed unnecessary using statements from CommentViewModel.cs and Post.cs. Reformatted the namespace declaration in CommentViewModel.cs for consistency and clarity. No functional changes to class implementations.

--- Blog/Models/Entities/Post.cs
+++ Blog/Models/Entities/Post.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Models.Entities
 {
--- Blog/Models/ViewModels/CommentViewModel.cs
+++ Blog/Models/ViewModels/CommentViewModel.cs
@@ -1,8 +1,4 @@
-﻿using Blog.Models.Entities;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Blog.Models.ViewModels
+﻿namespace Blog.Models.ViewModels
 {
     public class CommentViewModel : BaseViewModel
     {
