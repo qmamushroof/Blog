9809edd883bdc0127a3dd536211b0fd693daae54
Quazi Mushroof Abdullah <quazi.abdullah@studynetglobal.com>
2025-12-23 10:08:21 +0600

    add DB project

---
+++ DB/Tables/Category.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Category]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(400) NULL, 
+    [Slug] NVARCHAR(100) NOT NULL
+)
---
+++ DB/Tables/Post.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Post]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(300) NOT NULL, 
+    [Slug] NVARCHAR(300) NOT NULL, 
+    [Content] NVARCHAR(MAX) NOT NULL, 
+    [Status] INT NOT NULL, 
+    [Priority] INT NOT NULL, 
+    [PublishedAt] DATETIME NULL, 
+    [Deadline] DATETIME NULL, 
+    [HeaderImageUrl] VARCHAR(500) NULL, 
+    [CreatedAt] DATETIME NOT NULL, 
+    [UpdatedAt] DATETIME NOT NULL, 
+    [DeletedAt] DATETIME NULL, 
+    [CategoryId] INT NOT NULL, 
+    [AuthorId] VARCHAR(100) NULL, 
+    [ShareCount] INT NULL, 
+    CONSTRAINT [FK_Post_Category] FOREIGN KEY ([CategoryId]) REFERENCES [Category]([Id])
+)
