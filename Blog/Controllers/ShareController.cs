a4e30025e27485b5ee3949a5b3a7f72eaa94e321
Quazi Mushroof Abdullah <quazi.abdullah@studynetglobal.com>
2025-12-31 14:31:54 +0600

    Add ShareController

---
+++ Blog/Controllers/ShareController.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class ShareController : ControllerBase
+    {
+        private readonly IShareService _shareService;
+
+        public ShareController(IShareService shareService) => _shareService = shareService;
+    }
+}
